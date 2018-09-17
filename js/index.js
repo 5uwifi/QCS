f02b4947613d86257364036504dac34e16f45415
HengliYang <1029884642@qq.com>
2018-09-17 19:33:33 +0800

    fixxxx

--- js/index.js
+++ js/index.js
@@ -1,3 +1,25 @@
 $(document).ready(function () {
-
+    var fenpeiScrollTop = $('.code-allocation-wrap').offset().top;
+    var teamMemberScrollTop = $('.team-intro-wrap').offset().top;
+    var partnerBtnScrollTop = $('.partner-wrap').offset().top;
+    $('.fenpeiBtn').click(function () {
+        $('html,body').animate({ scrollTop: fenpeiScrollTop }, 500);
+    })
+    $('.teamMember').click(function () {
+        $('html,body').animate({ scrollTop: teamMemberScrollTop }, 500);
+    })
+    $('.partnerBtn').click(function () {
+        $('html,body').animate({ scrollTop: partnerBtnScrollTop }, 500);
+    })
+    $(window).scroll(function () {
+        if ($(window).scrollTop() >= 50) {
+            $('#back_btn_top').fadeIn();
+        }
+        else {
+            $('#back_btn_top').fadeOut();
+        }
+    });
+    $('#back_btn_top').click(function () {
+        $('html,body').animate({ scrollTop: 0 }, 500);
+    });
 })
