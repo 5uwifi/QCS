798c5c7cdd55a391485f358e09874039f2dcf27f
HengliYang <1029884642@qq.com>
2018-09-28 20:52:14 +0800

    new fix

--- js/index.js
+++ js/index.js
@@ -11,6 +11,15 @@ $(document).ready(function () {
     $('.partnerBtn').click(function () {
         $('html,body').animate({ scrollTop: partnerBtnScrollTop }, 500);
     })
+    $('.qqCode-bigwrap').on('click',function () {
+        $('.qq-qrcode-wrap').toggle();
+    })
+    $('.wxCode-bigwrap').on('click',function () {
+        $('.wx-qrcode-wrap').toggle();
+    })
+    $('.db-bigwrap').on('click',function () {
+        $('.db-qrcode-wrap').toggle();
+    })
     $(window).scroll(function () {
         if ($(window).scrollTop() >= 50) {
             $('#back_btn_top').fadeIn();
